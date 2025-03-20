f2d3ecfab9c978c93a226fd5071b5d8660a8d246
chengfushi <303623518@qq.com>
2025-03-20 18:31:18 +0800

    Learn

---
+++ Week04/src/com/generic_/method/CustomMethodGeneric.java
@@ -0,0 +1,18 @@
+package com.generic_.method;
+
+
+public class CustomMethodGeneric {
+    public static void main(String[] args) {
+        Apple<String,Integer,Double> apple = new Apple<>();
+        apple.show("111");
+
+    }
+}
+
+class Apple<T,R,E> {
+
+    public <E> void show(E e) {
+        System.out.println(e.getClass());
+        System.out.println(e);
+    }
+}
---
+++ Week04/src/com/generic_/method/GenericMethod.java
@@ -0,0 +1,50 @@
+package com.generic_.method;
+
+
+public class GenericMethod {
+    public static void main(String[] args) {
+        Person<String,Integer> person = new Person<>("小白",100);
+        person.show();
+
+    }
+}
+
+
+class Person<T,R> {
+    private T t;
+    private R r;
+
+    public Person(T t, R r) {
+        this.t = t;
+        this.r = r;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
+    public R getR() {
+        return r;
+    }
+
+    public void setR(R r) {
+        this.r = r;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "t=" + t +
+                ", r=" + r +
+                '}';
+    }
+
+    public <T,R> void show() {
+        System.out.println(t.getClass());
+        System.out.println(r.getClass());
+    }
+}
